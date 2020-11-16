6b13f8582f295682e06a851b8c0e412ec53f63d1
bobbyfromgeminus <info.geminus@gmail.com>
2020-11-16 02:32:26 +0100

    Javascript haladó 1 - Modal feladat kidolgozása

---
+++ _js/functions.js
@@ -0,0 +1,65 @@
+'use strict';
+
+
+// Modal ablak és overlay megjelenítése
+function showModal() {
+    let overlay = document.querySelectorAll('.overlay');
+    for (let i=0; i<overlay.length;i += 1) {
+        overlay[i].setAttribute('class', 'overlay_show');
+    
+    }
+    let modal = document.querySelectorAll('.modal');
+    for (let i=0; i<modal.length;i += 1) {
+        modal[i].setAttribute('class', 'modal_show');
+    }
+}
+
+// Modal ablak és overlay elrejtése
+function hideModal() {
+    let overlay = document.querySelectorAll('.overlay_show');
+    for (let i=0; i<overlay.length;i += 1) {
+        overlay[i].setAttribute('class', 'overlay_hide');
+        setTimeout(() => {
+            overlay[i].setAttribute('class', 'overlay');
+        }, 990);
+    }
+    let modal = document.querySelectorAll('.modal_show');
+    for (let i=0; i<modal.length;i += 1) {
+        modal[i].setAttribute('class', 'modal_hide');
+        setTimeout(() => {
+            modal[i].setAttribute('class', 'modal');
+        }, 990);
+    }
+}
+
+
+// Modal megjelenítő gomb eseménykezelője
+let modalButton = document.getElementById('modal-button');
+
+modalButton.addEventListener('click', function() {
+    showModal();
+});
+
+// Modal OK gomb eseménykezelője -> elrejtés
+let okButton = document.getElementById('modal-button_ok');
+okButton.addEventListener('click', function() {
+    hideModal();        
+});
+
+// Modal CANCEL gomb eseménykezelője -> elrejtés
+let cancelButton = document.getElementById('modal-button_cancel');
+cancelButton.addEventListener('click', function() {
+    hideModal();        
+});
+
+// Overlay eseménykezelője -> elrejtés
+let overlay = document.getElementById('overlay');
+overlay.addEventListener('click', function() {
+    hideModal();        
+});
+
+// Modal X gomb eseménykezelője -> elrejtés
+let modalClose = document.getElementById('modal-close');
+modalClose.addEventListener('click', function() {
+    hideModal();        
+});
