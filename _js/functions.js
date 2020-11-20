c6e379a3bd134d0a37711da86b9881f83674ca38
bobbyfromgeminus <info.geminus@gmail.com>
2020-11-20 13:42:29 +0100

    Hide animációt követő Display: none kezelése

--- _js/functions.js
+++ _js/functions.js
@@ -1,43 +1,23 @@
 'use strict';
 
-
-// Modal overlay megjelenítése
+/*
+-------------------------
+         OVERLAY
+-------------------------
+*/
+//      megjelenítése
 function showOverlay() {
     const overlay = document.querySelector('.overlay');
     overlay.setAttribute('class', 'overlay overlay_show');
 }
-
-// Modal ablak megjelenítése
-function showModal() {
-    const modal = document.querySelector('.modal');
-    modal.setAttribute('class', 'modal modal_show');
-}
-
-// Modal overlay elrejtése
+//      elrejtése
 function hideOverlay() {
     const overlay = document.querySelector('.overlay');
     overlay.setAttribute('class', 'overlay overlay_hide');
     const overlayEnd = document.querySelector('.overlay_hide');
     overlayEnd.addEventListener("webkitAnimationEnd", overlayDisplayNone);
 }
-
-// Modal ablak elrejtése
-function hideModal(state) {
-    const modal = document.querySelector('.modal');
-    modal.setAttribute('class', 'modal modal_hide');
-    const modalEnd = document.querySelector('.modal_hide');
-    modalEnd.addEventListener("webkitAnimationEnd", modalDisplayNone);
-    console.log(state);
-}
-
-// Modal ablak Diplay: none
-function modalDisplayNone() {
-    const modal = document.querySelector('.modal_hide');
-    modal.setAttribute('class', 'modal modal_hide modal_none');
-    modal.removeEventListener("webkitAnimationEnd", modalDisplayNone, false);
-}
-
-// Overlay Diplay: none
+//      Diplay: none
 function overlayDisplayNone() {
     const overlay = document.querySelector('.overlay_hide');
     overlay.setAttribute('class', 'overlay overlay_hide overlay_none');
@@ -45,42 +25,80 @@ function overlayDisplayNone() {
 }
 
 
+/*
+-------------------------
+      MODAL ABLAK
+-------------------------
+*/
+//      megjelenítése
+function showModal() {
+    const modal = document.querySelector('.modal');
+    modal.setAttribute('class', 'modal modal_show');
+}
+//      elrejtése
+function hideModal(state) {
+    const modal = document.querySelector('.modal');
+    modal.setAttribute('class', 'modal modal_hide');
+    const modalEnd = document.querySelector('.modal_hide');
+    modalEnd.addEventListener("webkitAnimationEnd", modalDisplayNone);
+    console.log(state);
+}
+//      Diplay: none
+function modalDisplayNone() {
+    const modal = document.querySelector('.modal_hide');
+    modal.setAttribute('class', 'modal modal_hide modal_none');
+    modal.removeEventListener("webkitAnimationEnd", modalDisplayNone, false);
+}
+
+
+/*
+-------------------------
+  SHOW/HIDE MEGHÍTÁSOK
+-------------------------
+*/
+//      megjelenítéshez
 function clickOpenModal() {
     showOverlay();
     showModal();
     const okButton = document.getElementById('modal-button_ok');
     okButton.focus();  
 }
-
+//      elrejtéshez
 function clickHideModal(state) {
     hideOverlay();
     hideModal(state);        
 }
 
-// Eseménykezelő a megjelenítéshez
+
+/*
+-------------------------
+     ESEMÉNYKEZELŐK
+-------------------------
+*/
+//      megjelenítéshez
 function openEventListener(id, state){
     const element = document.getElementById(id);
     element.addEventListener('click', () => clickOpenModal(state));
 }
-
-// Eseménykezelő az elrejtéshez
+//      elrejtéshez
 function hideEventListener(id, state){
     const element = document.getElementById(id);
     element.addEventListener('click', () => clickHideModal(state));
 }
 
 
+/*
+-------------------------
+ESEMÉNYKEZELŐK MEGHÍVÁSAI
+-------------------------
+*/
 // Modal megjelenítő gomb -> megjelenítés
 openEventListener('modal-button', true);
-
 // Modal OK gomb -> elrejtés
 hideEventListener('modal-button_ok', true);
-
 // Modal CANCEL gomb -> elrejtés
 hideEventListener('modal-button_cancel', false);
-
 // Overlay -> elrejtés
 hideEventListener('overlay', false);
-
 // Modal X gomb -> elrejtés
 hideEventListener('modal-close', false);
